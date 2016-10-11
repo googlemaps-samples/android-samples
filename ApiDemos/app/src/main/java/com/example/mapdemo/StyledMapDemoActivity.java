bb29e21b61d041886ce98cedf8d171d5252559a3
Stephen McDonald <steve@jupo.org>
2016-10-11 11:41:35 +1100

    Add Marker Info Window Close on Retap Demo
    Change-Id: Ie98c20ab2be5ce5f52289705524ebafc9db2f444

--- ApiDemos/app/src/main/java/com/example/mapdemo/StyledMapDemoActivity.java
+++ ApiDemos/app/src/main/java/com/example/mapdemo/StyledMapDemoActivity.java
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
