ceba3cb63729f55df863e14c9a63f5f3c300af24
Chris Arriola <carriola@google.com>
2020-05-07 18:58:12 -0700

    chore: Adding regiontag for MapsMarkerActivity

--- tutorials/java/MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
+++ tutorials/java/MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
@@ -27,9 +27,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
  */
+// [START maps_marker_on_map_ready]
 public class MapsMarkerActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
+    // [START_EXCLUDE]
+    // [START maps_marker_get_map_async]
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +44,10 @@ public class MapsMarkerActivity extends AppCompatActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    // [END maps_marker_get_map_async]
+    // [END_EXCLUDE]
 
+    // [START_EXCLUDE silent]
     /**
      * Manipulates the map when it's available.
      * The API invokes this callback when the map is ready to be used.
@@ -51,6 +57,7 @@ public class MapsMarkerActivity extends AppCompatActivity
      * Play services inside the SupportMapFragment. The API invokes this method after the user has
      * installed Google Play services and returned to the app.
      */
+    // [END_EXCLUDE silent]
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
@@ -61,3 +68,4 @@ public class MapsMarkerActivity extends AppCompatActivity
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
+// [END maps_marker_on_map_ready]
