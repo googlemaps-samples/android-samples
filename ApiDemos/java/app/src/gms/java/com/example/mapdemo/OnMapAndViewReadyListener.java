fc64eca1512c569034fee09aa8b1d529f24a3d49
Chris Arriola <carriola@google.com>
2020-05-12 17:15:09 -0700

    chore: ApiDemos for Maps SDK V3 BETA (#199)
    * chore: Adding samples for V3.
    
    Adding samples for V3 BETA in a new module `app-v3`. These samples are
    generated by the `generateV3` Gradle task which runs when the `app`
    module is built.
    
    TODOs left:
    - [ ] Update README with a note about V3 samples
    
    * Update README and location of secure.properties.
    
    * Using product flavors instead of creating a new module for V3 & GMS.
    
    * Update build workflow to run lint and assemble on all flavor/build types.
    
    * Not using deprecated API.
    
    * Install Android NDK.
    
    * Update license headers.

---
+++ ApiDemos/java/app/src/gms/java/com/example/mapdemo/OnMapAndViewReadyListener.java
@@ -0,0 +1,100 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.example.mapdemo;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Helper class that will delay triggering the OnMapReady callback until both the GoogleMap and the
+ * View having completed initialization. This is only necessary if a developer wishes to immediately
+ * invoke any method on the GoogleMap that also requires the View to have finished layout
+ * (ie. anything that needs to know the View's true size like snapshotting).
+ */
+public class OnMapAndViewReadyListener implements OnGlobalLayoutListener, OnMapReadyCallback {
+
+    /** A listener that needs to wait for both the GoogleMap and the View to be initialized. */
+    public interface OnGlobalLayoutAndMapReadyListener {
+        void onMapReady(GoogleMap googleMap);
+    }
+
+    private final SupportMapFragment mapFragment;
+    private final View mapView;
+    private final OnGlobalLayoutAndMapReadyListener devCallback;
+
+    private boolean isViewReady;
+    private boolean isMapReady;
+    private GoogleMap googleMap;
+
+    public OnMapAndViewReadyListener(
+            SupportMapFragment mapFragment, OnGlobalLayoutAndMapReadyListener devCallback) {
+        this.mapFragment = mapFragment;
+        mapView = mapFragment.getView();
+        this.devCallback = devCallback;
+        isViewReady = false;
+        isMapReady = false;
+        googleMap = null;
+
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        // View layout.
+        if ((mapView.getWidth() != 0) && (mapView.getHeight() != 0)) {
+            // View has already completed layout.
+            isViewReady = true;
+        } else {
+            // Map has not undergone layout, register a View observer.
+            mapView.getViewTreeObserver().addOnGlobalLayoutListener(this);
+        }
+
+        // GoogleMap. Note if the GoogleMap is already ready it will still fire the callback later.
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // NOTE: The GoogleMap API specifies the listener is removed just prior to invocation.
+        this.googleMap = googleMap;
+        isMapReady = true;
+        fireCallbackIfReady();
+    }
+
+    @SuppressWarnings("deprecation")  // We use the new method when supported
+    @SuppressLint("NewApi")  // We check which build version we are using.
+    @Override
+    public void onGlobalLayout() {
+        // Remove our listener.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+        } else {
+            mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        }
+        isViewReady = true;
+        fireCallbackIfReady();
+    }
+
+    private void fireCallbackIfReady() {
+        if (isViewReady && isMapReady) {
+            devCallback.onMapReady(googleMap);
+        }
+    }
+}
---
+++ ApiDemos/java/app/src/gms/java/com/example/mapdemo/StyledMapDemoActivity.java
@@ -0,0 +1,189 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package com.example.mapdemo;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This shows how to style a map with JSON.
+ */
+public class StyledMapDemoActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap = null;
+
+    private static final String TAG = StyledMapDemoActivity.class.getSimpleName();
+
+    private static final String SELECTED_STYLE = "selected_style";
+
+    // Stores the ID of the currently selected style, so that we can re-apply it when
+    // the activity restores state, for example when the device changes orientation.
+    private int mSelectedStyleId = R.string.style_label_default;
+
+    // These are simply the string resource IDs for each of the style names. We use them
+    // as identifiers when choosing which style to apply.
+    private int mStyleIds[] = {
+            R.string.style_label_retro,
+            R.string.style_label_night,
+            R.string.style_label_grayscale,
+            R.string.style_label_no_pois_no_transit,
+            R.string.style_label_default,
+    };
+
+    private static final LatLng SYDNEY = new LatLng(-33.8688, 151.2093);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mSelectedStyleId = savedInstanceState.getInt(SELECTED_STYLE);
+        }
+        setContentView(R.layout.styled_map_demo);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Store the selected map style, so we can assign it when the activity resumes.
+        outState.putInt(SELECTED_STYLE, mSelectedStyleId);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 14));
+        setSelectedStyle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.styled_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_style_choose) {
+            showStylesDialog();
+        }
+        return true;
+    }
+
+    /**
+     * Shows a dialog listing the styles to choose from, and applies the selected
+     * style when chosen.
+     */
+    private void showStylesDialog() {
+        // mStyleIds stores each style's resource ID, and we extract the names here, rather
+        // than using an XML array resource which AlertDialog.Builder.setItems() can also
+        // accept. We do this since using an array resource would mean we would not have
+        // constant values we can switch/case on, when choosing which style to apply.
+        List<String> styleNames = new ArrayList<>();
+        for (int style : mStyleIds) {
+            styleNames.add(getString(style));
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.style_choose));
+        builder.setItems(styleNames.toArray(new CharSequence[styleNames.size()]),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mSelectedStyleId = mStyleIds[which];
+                        String msg = getString(R.string.style_set_to, getString(mSelectedStyleId));
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                        setSelectedStyle();
+                    }
+                });
+        builder.show();
+    }
+
+    /**
+     * Creates a {@link MapStyleOptions} object via loadRawResourceStyle() (or via the
+     * constructor with a JSON String), then sets it on the {@link GoogleMap} instance,
+     * via the setMapStyle() method.
+     */
+    private void setSelectedStyle() {
+        MapStyleOptions style;
+        switch (mSelectedStyleId) {
+            case R.string.style_label_retro:
+                // Sets the retro style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_retro);
+                break;
+            case R.string.style_label_night:
+                // Sets the night style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_night);
+                break;
+            case R.string.style_label_grayscale:
+                // Sets the grayscale style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_grayscale);
+                break;
+            case R.string.style_label_no_pois_no_transit:
+                // Sets the no POIs or transit style via JSON string.
+                style = new MapStyleOptions("[" +
+                        "  {" +
+                        "    \"featureType\":\"poi.business\"," +
+                        "    \"elementType\":\"all\"," +
+                        "    \"stylers\":[" +
+                        "      {" +
+                        "        \"visibility\":\"off\"" +
+                        "      }" +
+                        "    ]" +
+                        "  }," +
+                        "  {" +
+                        "    \"featureType\":\"transit\"," +
+                        "    \"elementType\":\"all\"," +
+                        "    \"stylers\":[" +
+                        "      {" +
+                        "        \"visibility\":\"off\"" +
+                        "      }" +
+                        "    ]" +
+                        "  }" +
+                        "]");
+                break;
+            case R.string.style_label_default:
+                // Removes previously set style, by setting it to null.
+                style = null;
+                break;
+            default:
+                return;
+        }
+        mMap.setMapStyle(style);
+    }
+
+}
