68917831150e5dc8f569538e67d752141eb65c00
Chris Arriola <carriola@google.com>
2020-05-12 17:15:09 -0700

    chore: ApiDemos for Maps SDK V3 BETA (#199)
    * chore: Adding samples for V3.
    
    Adding samples for V3 BETA in a new module `app-v3`. These samples are
    generated by the `generateV3` Gradle task which runs when the `app`
    module is built.
    
    TODOs left:
    - [ ] Update README with a note about V3 samples
    
    * Update README and location of secure.properties.
    
    * Using product flavors instead of creating a new module for V3 & GMS.
    
    * Update build workflow to run lint and assemble on all flavor/build types.
    
    * Not using deprecated API.
    
    * Install Android NDK.
    
    * Update license headers.

--- ApiDemos/java/app/src/main/java/com/example/mapdemo/MapInPagerDemoActivity.java
+++ ApiDemos/java/app/src/main/java/com/example/mapdemo/MapInPagerDemoActivity.java
@@ -1,18 +1,17 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 package com.example.mapdemo;
 
@@ -34,23 +33,19 @@ import androidx.viewpager.widget.ViewPager;
  */
 public class MapInPagerDemoActivity extends AppCompatActivity {
 
-    private MyAdapter mAdapter;
-
-    private ViewPager mPager;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_in_pager_demo);
-        mAdapter = new MyAdapter(getSupportFragmentManager());
+        MyAdapter adapter = new MyAdapter(getSupportFragmentManager());
 
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPager.setAdapter(mAdapter);
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(adapter);
 
         // This is required to avoid a black flash when the map is loaded.  The flash is due
         // to the use of a SurfaceView as the underlying view of the map.
-        mPager.requestTransparentRegion(mPager);
+        pager.requestTransparentRegion(pager);
     }
 
     /** A simple fragment that displays a TextView. */
@@ -66,7 +61,7 @@ public class MapInPagerDemoActivity extends AppCompatActivity {
     public static class MyAdapter extends FragmentPagerAdapter {
 
         public MyAdapter(FragmentManager fm) {
-            super(fm);
+            super(fm, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         }
 
         @Override
