b2c83b643e98e6d7f759d3370690c1ec4e2b9285
Sean Barbeau <sjbarbeau@gmail.com>
2020-05-15 14:32:15 -0400

    chore: Add SplitStreetViewPanoramaAndMapDemoActivity in Kotlin (#203)
    Also update Java example to not use Hungarian notation

--- ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/DemoDetailsList.kt
+++ ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/DemoDetailsList.kt
@@ -45,6 +45,10 @@ class DemoDetailsList {
                         R.string.street_view_panorama_navigation_demo_label,
                         R.string.street_view_panorama_navigation_demo_details,
                         StreetViewPanoramaNavigationDemoActivity::class.java),
+                DemoDetails(
+                        R.string.split_street_view_panorama_and_map_demo_label,
+                        R.string.split_street_view_panorama_and_map_demo_details,
+                        SplitStreetViewPanoramaAndMapDemoActivity::class.java),
                 DemoDetails(R.string.tags_demo_label, R.string.tags_demo_details,
                         TagsDemoActivity::class.java),
                 DemoDetails(R.string.ui_settings_demo_label, R.string.ui_settings_demo_details,
