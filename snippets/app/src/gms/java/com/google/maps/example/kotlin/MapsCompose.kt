9a8a43b2dbed31ae348b168f579d432349bd86fa
Chris Arriola <chrisarriola@google.com>
2022-03-21 16:02:48 -0700

    Bump to gradle 7.2.
    Change-Id: Ia36fbf147bc3ec8c5f44fd980868a691263174e9

--- snippets/app/src/gms/java/com/google/maps/example/kotlin/MapsCompose.kt
+++ snippets/app/src/gms/java/com/google/maps/example/kotlin/MapsCompose.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -17,7 +16,7 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
-import java.util.Properties
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun AddAMap() {
