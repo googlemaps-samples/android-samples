f055c3ad15a9992de692ecbdc5b70fe89c22c27b
Angela Yu <5506675+wangela@users.noreply.github.com>
2023-10-28 02:38:51 -0700

    chore: create app-ktx snippets module

--- snippets/app-ktx/src/main/java/com/example/app_ktx/KTX.kt
+++ snippets/app-ktx/src/main/java/com/example/app_ktx/KTX.kt
@@ -1,27 +1,31 @@
-package com.google.maps.example.kotlin
+package com.example.app_ktx
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.ktx.addMarker
 import com.google.maps.android.ktx.awaitMap
 import com.google.maps.android.ktx.cameraMoveEvents
-import com.google.maps.example.R
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 internal class KTX : AppCompatActivity() {
   private lateinit var googleMap: GoogleMap
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     // [START maps_android_ktx_obtain_map]
-    lifecycleScope.launchWhenCreated {
-      val mapFragment: SupportMapFragment? =
-        supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-      val googleMap: GoogleMap? = mapFragment?.awaitMap()
+    lifecycleScope.launch {
+      lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+        val mapFragment: SupportMapFragment? =
+          supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        val googleMap: GoogleMap? = mapFragment?.awaitMap()
+      }
     }
     // [END maps_android_ktx_obtain_map]
 
@@ -35,9 +39,11 @@ internal class KTX : AppCompatActivity() {
     // [END maps_android_ktx_add_marker]
 
     // [START maps_android_ktx_camera_events]
-    lifecycleScope.launchWhenCreated {
-      googleMap.cameraMoveEvents().collect {
-        print("Received camera move event")
+    lifecycleScope.launch {
+      lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+        googleMap.cameraMoveEvents().collect {
+          print("Received camera move event")
+        }
       }
     }
     // [END maps_android_ktx_camera_events]
