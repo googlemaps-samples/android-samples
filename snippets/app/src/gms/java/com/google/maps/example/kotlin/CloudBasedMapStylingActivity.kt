abcc8b52c2246ebc5b74865be9e69e89c95c62cd
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2024-07-24 10:56:55 -0600

    feat: copies some snippets that were in v3 only to gms (#1715)
    * feat: copies some snippets that were in v3 only to gms
    chore: the java compatibility version
    
    * fix: updates copyright dates and adds missing copyright
    
    * fix: copyright header for strings.xml
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * feat: creates AdvancedMarkersCollisionActivity to demonstrate marker collision behavior
    
    * chore: not adding jar files in libs directory
    
    * feat: set Java 21 everywhere
    
    ---------
    
    Co-authored-by: Enrique López Mañas <eenriquelopez@gmail.com>

---
+++ snippets/app/src/gms/java/com/google/maps/example/kotlin/CloudBasedMapStylingActivity.kt
@@ -0,0 +1,33 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.maps.example.kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.MapFragment
+import com.google.maps.example.R
+
+class CloudBasedMapStylingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // [START maps_android_cloud_based_map_styling]
+        val mapFragment = MapFragment.newInstance(
+            GoogleMapOptions()
+                .mapId(resources.getString(R.string.map_id))
+        )
+        // [END maps_android_cloud_based_map_styling]
+    }
+}
