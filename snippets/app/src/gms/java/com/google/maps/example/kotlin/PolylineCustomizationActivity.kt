59d32e4c883dcdc97c922bf85491dce41bff4932
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2024-07-24 10:56:55 -0600

    feat: copies some snippets that were in v3 only to gms (#1715)
    * feat: copies some snippets that were in v3 only to gms
    chore: the java compatibility version
    
    * fix: updates copyright dates and adds missing copyright
    
    * fix: copyright header for strings.xml
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * fix: set Java Version back to 17
    
    * feat: creates AdvancedMarkersCollisionActivity to demonstrate marker collision behavior
    
    * chore: not adding jar files in libs directory
    
    * feat: set Java 21 everywhere
    
    ---------
    
    Co-authored-by: Enrique López Mañas <eenriquelopez@gmail.com>

---
+++ snippets/app/src/gms/java/com/google/maps/example/kotlin/PolylineCustomizationActivity.kt
@@ -0,0 +1,71 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.maps.example.kotlin
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.StrokeStyle
+import com.google.android.gms.maps.model.StyleSpan
+import com.google.android.gms.maps.model.TextureStyle
+import com.google.maps.example.R
+
+class PolylineCustomizationActivity : AppCompatActivity() {
+    private lateinit var map: GoogleMap
+
+    private fun multicoloredPolyline() {
+        // [START maps_android_polyline_multicolored]
+        val line = map.addPolyline(
+            PolylineOptions()
+                .add(LatLng(47.6677146, -122.3470447), LatLng(47.6442757, -122.2814693))
+                .addSpan(StyleSpan(Color.RED))
+                .addSpan(StyleSpan(Color.GREEN))
+        )
+        // [END maps_android_polyline_multicolored]
+    }
+
+    private fun multicoloredGradientPolyline() {
+        // [START maps_android_polyline_gradient]
+        val line = map.addPolyline(
+            PolylineOptions()
+                .add(LatLng(47.6677146, -122.3470447), LatLng(47.6442757, -122.2814693))
+                .addSpan(
+                    StyleSpan(
+                        StrokeStyle.gradientBuilder(
+                            Color.RED,
+                            Color.YELLOW
+                        ).build()
+                    )
+                )
+        )
+        // [END maps_android_polyline_gradient]
+    }
+
+    private fun stampedPolyline() {
+        // [START maps_android_polyline_stamped]
+        val stampStyle =
+            TextureStyle.newBuilder(BitmapDescriptorFactory.fromResource(R.drawable.walking_dot)).build()
+        val span = StyleSpan(StrokeStyle.colorBuilder(Color.RED).stamp(stampStyle).build())
+        map.addPolyline(
+            PolylineOptions()
+                .add(LatLng(47.6677146, -122.3470447), LatLng(47.6442757, -122.2814693))
+                .addSpan(span)
+        )
+        // [END maps_android_polyline_stamped]
+    }
+}
