f8a20e99b8c0024634ca6e7ef68710adef41168a
Enrique López Mañas <eenriquelopez@gmail.com>
2024-08-09 18:43:23 +0200

    feat: restored files in GMS

---
+++ ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/AdvancedMarkersDemoActivity.kt
@@ -0,0 +1,140 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.example.kotlindemos
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.OnMapReadyCallback
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.GoogleMap
+import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.AdvancedMarkerOptions
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapCapabilities
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.PinConfig
+
+
+private val SINGAPORE = LatLng(1.3521, 103.8198)
+private val KUALA_LUMPUR = LatLng(3.1390, 101.6869)
+private val JAKARTA = LatLng(-6.2088, 106.8456)
+private val BANGKOK = LatLng(13.7563, 100.5018)
+private val MANILA = LatLng(14.5995, 120.9842)
+private val HO_CHI_MINH_CITY = LatLng(10.7769, 106.7009)
+
+const val ZOOM_LEVEL = 3.5f
+
+private val TAG = AdvancedMarkersDemoActivity::class.java.name
+
+
+/**
+ * The following sample showcases how to create Advanced Markers, and use all their customization
+ * possibilities.
+ */
+// [START maps_android_sample_marker_advanced]
+class AdvancedMarkersDemoActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.advanced_markers_demo)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+
+        with(map) {
+            moveCamera(CameraUpdateFactory.newLatLngZoom(SINGAPORE, ZOOM_LEVEL))
+        }
+
+        val capabilities: MapCapabilities = map.mapCapabilities
+        Log.d(TAG, "are advanced marker enabled?" + capabilities.isAdvancedMarkersAvailable)
+
+        // This sample sets a view as the iconView for the Advanced Marker
+        val textView = TextView(this)
+        textView.text = "Hello!"
+        val advancedMarkerView: Marker? = map.addMarker(
+            AdvancedMarkerOptions().position(SINGAPORE).iconView(textView).zIndex(1f)
+        )
+
+        // This uses PinConfig.Builder to create an instance of PinConfig.
+        val pinConfigBuilder: PinConfig.Builder = PinConfig.builder()
+        pinConfigBuilder.setBackgroundColor(Color.MAGENTA)
+        val pinConfig: PinConfig = pinConfigBuilder.build()
+
+
+        // Use the  PinConfig instance to set the icon for AdvancedMarkerOptions.
+        val advancedMarkerOptions: AdvancedMarkerOptions =
+            AdvancedMarkerOptions().icon(BitmapDescriptorFactory.fromPinConfig(pinConfig))
+                .position(KUALA_LUMPUR)
+
+
+        // Pass the AdvancedMarkerOptions instance to addMarker().
+        val marker: Marker? = map.addMarker(advancedMarkerOptions)
+
+        // This sample changes the border color of the advanced marker
+        val pinConfigBuilder2: PinConfig.Builder = PinConfig.builder()
+        pinConfigBuilder2.setBorderColor(Color.BLUE)
+        val pinConfig2: PinConfig = pinConfigBuilder2.build()
+
+        val advancedMarkerOptions2: AdvancedMarkerOptions = AdvancedMarkerOptions()
+            .icon(BitmapDescriptorFactory.fromPinConfig(pinConfig2))
+            .position(JAKARTA)
+
+
+        val marker2: Marker? = map.addMarker(advancedMarkerOptions2)
+
+        // Set the glyph text.
+        val pinConfigBuilder3: PinConfig.Builder = PinConfig.builder()
+        val glyphText = PinConfig.Glyph("A")
+
+        // Alternatively, you can set the text color:
+        // Glyph glyphText = new Glyph("A", Color.GREEN);
+        pinConfigBuilder3.setGlyph(glyphText)
+        val pinConfig3: PinConfig = pinConfigBuilder3.build()
+
+        val advancedMarkerOptions3: AdvancedMarkerOptions = AdvancedMarkerOptions()
+            .icon(BitmapDescriptorFactory.fromPinConfig(pinConfig3))
+            .position(BANGKOK)
+
+        val marker3: Marker? = map.addMarker(advancedMarkerOptions3)
+
+        // Create a transparent glyph.
+        val pinConfigBuilder4: PinConfig.Builder = PinConfig.builder()
+        pinConfigBuilder4.setBackgroundColor(Color.MAGENTA)
+        pinConfigBuilder4.setGlyph(PinConfig.Glyph(Color.TRANSPARENT))
+        val pinConfig4: PinConfig = pinConfigBuilder4.build()
+
+        val advancedMarkerOptions4: AdvancedMarkerOptions = AdvancedMarkerOptions()
+            .icon(BitmapDescriptorFactory.fromPinConfig(pinConfig4))
+            .position(MANILA)
+
+        val marker4: Marker? = map.addMarker(advancedMarkerOptions4)
+
+        // Collision behavior can only be changed in the AdvancedMarkerOptions object.
+        // Changes to collision behavior after a marker has been created are not possible
+        val collisionBehavior: Int =
+            AdvancedMarkerOptions.CollisionBehavior.REQUIRED_AND_HIDES_OPTIONAL
+        val advancedMarkerOptions5: AdvancedMarkerOptions = AdvancedMarkerOptions()
+            .position(HO_CHI_MINH_CITY)
+            .collisionBehavior(collisionBehavior)
+
+        val marker5: Marker? = map.addMarker(advancedMarkerOptions5)
+    }
+}
+// [END maps_android_sample_marker_advanced]
---
+++ ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/SaveStateDemoActivity.kt
@@ -0,0 +1,160 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.example.kotlindemos
+
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.maps.android.ktx.CameraEvent
+import com.google.maps.android.ktx.addMarker
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.cameraEvents
+import kotlinx.android.parcel.Parcelize
+import kotlinx.coroutines.flow.collect
+import java.util.Random
+
+/**
+ * This activity shows how to save the state of a MapFragment when the activity is recreated, like
+ * after rotation of the device.
+ */
+class SaveStateDemoActivity : AppCompatActivity() {
+
+  @Parcelize
+  internal data class MarkerInfo(var hue: Float) : Parcelable
+
+  /**
+   * Example of a custom `MapFragment` showing how the position of a marker and other
+   * custom
+   * [Parcelable]s objects can be saved after rotation of the device.
+   *
+   *
+   * Storing custom [Parcelable] objects directly in the [Bundle] provided by the
+   * [.onActivityCreated] method will throw a `ClassNotFoundException`. This
+   * is due to the fact that this Bundle is parceled (thus losing its ClassLoader attribute at
+   * this moment) and unparceled later in a different ClassLoader.
+   * <br></br>
+   * A workaround to store these objects is to wrap the custom [Parcelable] objects in a
+   * new
+   * [Bundle] object.
+   *
+   *
+   * However, note that it is safe to store [Parcelable] objects from the Maps API (eg.
+   * MarkerOptions, LatLng, etc.) directly in the Bundle provided by the
+   * [.onActivityCreated] method.
+   */
+  class SaveStateMapFragment : SupportMapFragment(), OnMarkerClickListener, OnMarkerDragListener {
+
+    private lateinit var mMarkerPosition: LatLng
+    private lateinit var mMarkerInfo: MarkerInfo
+    private var mMoveCameraToMarker = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+
+      // Extract the state of the MapFragment:
+      // - Objects from the API (eg. LatLng, MarkerOptions, etc.) were stored directly in
+      //   the savedInsanceState Bundle.
+      // - Custom Parcelable objects were wrapped in another Bundle.
+      mMarkerPosition =
+        savedInstanceState?.getParcelable(MARKER_POSITION) ?: DEFAULT_MARKER_POSITION
+      mMarkerInfo =
+        savedInstanceState?.getBundle(OTHER_OPTIONS)?.getParcelable(MARKER_INFO) ?: MarkerInfo(
+          BitmapDescriptorFactory.HUE_RED)
+      mMoveCameraToMarker = savedInstanceState == null
+
+      lifecycleScope.launchWhenCreated {
+        val map = awaitMap()
+        map.addMarker {
+          icon(BitmapDescriptorFactory.defaultMarker(mMarkerInfo.hue))
+          position(mMarkerPosition)
+          draggable(true)
+        }
+
+        map.setOnMarkerDragListener(this@SaveStateMapFragment)
+        map.setOnMarkerClickListener(this@SaveStateMapFragment)
+
+        if (mMoveCameraToMarker) {
+          map.animateCamera(CameraUpdateFactory.newLatLng(mMarkerPosition))
+        }
+      }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+      super.onSaveInstanceState(outState)
+
+      // All Parcelable objects of the API  (eg. LatLng, MarkerOptions, etc.) can be set
+      // directly in the given Bundle.
+      outState.putParcelable(MARKER_POSITION, mMarkerPosition)
+
+      // All other custom Parcelable objects must be wrapped in another Bundle. Indeed,
+      // failing to do so would throw a ClassNotFoundException. This is due to the fact that
+      // this Bundle is being parceled (losing its ClassLoader at this time) and unparceled
+      // later in a different ClassLoader.
+      val bundle = Bundle()
+      bundle.putParcelable(MARKER_INFO, mMarkerInfo)
+      outState.putBundle(OTHER_OPTIONS, bundle)
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+      val newHue = MARKER_HUES[Random()
+        .nextInt(MARKER_HUES.size)]
+      mMarkerInfo.hue = newHue
+      marker.setIcon(BitmapDescriptorFactory.defaultMarker(newHue))
+      return true
+    }
+
+    override fun onMarkerDragStart(marker: Marker) {}
+    override fun onMarkerDrag(marker: Marker) {}
+    override fun onMarkerDragEnd(marker: Marker) {
+      mMarkerPosition = marker.position
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.save_state_demo)
+  }
+
+  companion object {
+    /** Default marker position when the activity is first created.  */
+    private val DEFAULT_MARKER_POSITION = LatLng(48.858179, 2.294576)
+
+    /** List of hues to use for the marker  */
+    private val MARKER_HUES = floatArrayOf(
+      BitmapDescriptorFactory.HUE_RED,
+      BitmapDescriptorFactory.HUE_ORANGE,
+      BitmapDescriptorFactory.HUE_YELLOW,
+      BitmapDescriptorFactory.HUE_GREEN,
+      BitmapDescriptorFactory.HUE_CYAN,
+      BitmapDescriptorFactory.HUE_AZURE,
+      BitmapDescriptorFactory.HUE_BLUE,
+      BitmapDescriptorFactory.HUE_VIOLET,
+      BitmapDescriptorFactory.HUE_MAGENTA,
+      BitmapDescriptorFactory.HUE_ROSE)
+
+    // Bundle keys.
+    private const val OTHER_OPTIONS = "options"
+    private const val MARKER_POSITION = "markerPosition"
+    private const val MARKER_INFO = "markerInfo"
+  }
+}
