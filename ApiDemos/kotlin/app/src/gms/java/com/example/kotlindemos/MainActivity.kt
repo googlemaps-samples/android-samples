8e47cfbe21e6e701c362e71cd9fd51c0b6bd2f69
Angela Yu <5506675+wangela@users.noreply.github.com>
2024-08-16 12:26:28 -0700

    chore: update build.gradle comments for adding an API key (#1756)

--- ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/MainActivity.kt
+++ ApiDemos/kotlin/app/src/gms/java/com/example/kotlindemos/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         }
 
         if (BuildConfig.MAPS_API_KEY.isEmpty()) {
-            Toast.makeText(this, "Add your own API key in local.properties as MAPS_API_KEY=YOUR_API_KEY", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Add your own API key in secrets.properties as MAPS_API_KEY=YOUR_API_KEY", Toast.LENGTH_LONG).show()
         }
     }
 
