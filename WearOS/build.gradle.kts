c772e7eab88379d7e66db2377d61105d7f038014
Enrique López Mañas <eenriquelopez@gmail.com>
2024-08-18 18:39:21 +0200

    feat: tooling modernization

--- WearOS/build.gradle.kts
+++ WearOS/build.gradle.kts
@@ -15,13 +15,7 @@
  */
 
 plugins {
-<<<<<<< HEAD:WearOS/build.gradle
-    id 'com.android.application' version '8.5.2' apply false
-    id 'com.google.android.libraries.mapsplatform.secrets-gradle-plugin' version '2.0.1' apply false
-    id 'org.jetbrains.kotlin.android' version '2.0.0' apply false
-=======
     id("com.android.application") version "8.5.0" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
->>>>>>> cfcf96fd (feat: tooling modernization):WearOS/build.gradle.kts
 }
