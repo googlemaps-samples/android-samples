561e82926e30c5862f1b69666346a387917492c1
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2024-09-19 11:16:35 -0600

    fix: always use secrets.properties for storing the API keys (#1812)

--- tutorials/kotlin/CurrentPlaceDetailsOnMap/app/build.gradle.kts
+++ tutorials/kotlin/CurrentPlaceDetailsOnMap/app/build.gradle.kts
@@ -69,6 +69,13 @@ dependencies {
 }
 
 secrets {
+    // To add your Maps API key to this project:
+    // 1. If the secrets.properties file does not exist, create it in the same folder as the local.properties file.
+    // 2. Add this line, where YOUR_API_KEY is your API key:
+    //        MAPS_API_KEY=YOUR_API_KEY
     propertiesFileName = "secrets.properties"
+
+    // A properties file containing default secret values. This file can be
+    // checked in version control.
     defaultPropertiesFileName = "local.defaults.properties"
 }
