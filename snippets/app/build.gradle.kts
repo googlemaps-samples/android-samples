95f70aafdd29ec59c517b49990a5585ec54ad46b
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2025-01-28 09:15:00 -0700

    chore: bump versions for gradle (8.8.0), kotlin(2.1.0), the targetSd… (#1950)
    chore: bump versions for gradle (8.8.0), kotlin(2.1.0)), the targetSdk(35) and other deps

--- snippets/app/build.gradle.kts
+++ snippets/app/build.gradle.kts
@@ -96,10 +96,5 @@ secrets {
     // A properties file containing default secret values. This file can be
     // checked in version control.
     defaultPropertiesFileName = "local.defaults.properties"
-
-    // Configure which keys should be ignored by the plugin by providing regular expressions.
-    // "sdk.dir" is ignored by default.
-    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
-    ignoreList.add("sdk.*")       // Ignore all keys matching the regexp "sdk.*"
 }
 // [END maps_android_secrets_gradle_plugin_config]
